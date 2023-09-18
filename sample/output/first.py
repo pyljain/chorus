3ab413024e48ada6707c71fe54b697979dc5881d
Payal Jain <pyl.jain3@gmail.com>
2023-09-18 18:03:19 +0100

    Updated README

--- sample/output/first.py
+++ sample/output/first.py
@@ -1,16 +1,9 @@
 def add_numbers(num1, num2):
-    """
-    Calculate the sum of two numbers and print the result.
-    
-    Parameters:
-        num1 (int): The first number
-        num2 (int): The second number
-    """
-    sum_value = num1 + num2
+    # Declare a local variable to hold the sum
+    total = num1 + num2
 
     # Print the result
-    print(f'The sum of {num1} and {num2} is {sum_value}.')
-
+    print(f'The sum of {num1} and {num2} is {total}')
 
 if __name__ == '__main__':
-    add_numbers(10, 20)
+    add_numbers(5, 10)
--- sample/output/first_test.py
+++ sample/output/first_test.py
@@ -1,26 +1,17 @@
 import unittest
-from io import StringIO
+import io
 import sys
 from first import add_numbers
 
-
 class Testing(unittest.TestCase):
-    def test_add_numbers(self):
-        expected_output = 'The sum of 10 and 20 is 30.'
-
-        # Redirect standard output to StringIO
-        output = StringIO()
-        sys.stdout = output
-
-        # Call the function
-        add_numbers(10, 20)
-
-        # Get the output
-        outputStr = output.getvalue().strip()
-
-        # Check the output
-        self.assertEqual(outputStr, expected_output)
-
+    def test_addNumbers(self):
+        capturedOutput = io.StringIO()  # Create StringIO object
+        sys.stdout = capturedOutput  # Redirect stdout to StringIO object
+        add_numbers(5, 10)  # Call the function
+        sys.stdout = sys.__stdout__  # Reset redirect
+        output = capturedOutput.getvalue().strip()  # Get the captured output
+        expected_output = 'The sum of 5 and 10 is 15'
+        self.assertEqual(output, expected_output)  # Check if the output matches the expected output
 
 if __name__ == '__main__':
     unittest.main()
